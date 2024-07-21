a1997ab12616651c3838f1986bcc0c1a056cfd03
Aryan KachhiyaPatel <kachhary.sheridancollege.ca>
2024-07-21 18:41:43 -0400

    Created a code for BlackJack game and UML diagram.

---
+++ src/blackjack/Card.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blackjack;
+
+/**
+ *
+ * @author Aayush Soni,Aryan Kachhiyapatel,Dev Patel,Tanish Vyas|
+ */
+
+
+public class Card {
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
+
